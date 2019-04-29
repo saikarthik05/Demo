b9e4107daad0122a3762a9bdef5c58fcd60429bb
KARTHIK <KARTHIK@LAPTOP-Q7M96OO3>
2019-04-29 09:01:09 +0530

    filtering code

--- restfulwebservices/src/main/java/com/myproject/restfulwebservices/helloworld/FilteringController.java
+++ restfulwebservices/src/main/java/com/myproject/restfulwebservices/helloworld/FilteringController.java
@@ -1,7 +1,12 @@
 package com.myproject.restfulwebservices.helloworld;
 
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 import filtering.FilteringBean;
 
@@ -11,7 +16,15 @@ public class FilteringController {
 	@GetMapping("/Filtering")
 	public FilteringBean retriveFilteringBean1() {
 		
-		return new FilteringBean("val1","val2","val3");
+		FilteringBean someBean = new FilteringBean("karthik", "sravani", "bujji");
+	/*	SimpleBeanPropertyFilter filter = new SimpleBeanPropertyFilter.FilterOutAllExcept("field2","field1");
+		FilterProvider filters = new SimpleFilterProvider().addFilter("someBean", filter);
+		MappingJacksonValue mapping = new MappingJacksonValue(someBean);
+		mapping.setFilters(filters);*/
+		
+	
+		
+		return someBean;
 		
 	}
 }
